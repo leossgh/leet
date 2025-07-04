fdcec43edb131bbe5d704efe8e74d20ceae60cb1
leo <leonarss@stude.ntnu.no>
2025-07-04 09:29:28 +0200

    added normal solution to 1342 in addition to recursive solution

--- Easy/1342.py
+++ Easy/1342.py
@@ -15,7 +15,7 @@ class PythonSolution(object):
         """
         return self.recursive(PythonSolution,num,0)
     
-class Solution(object):
+class RecursiveSolution(object):
     def recursive(self,num,steps):
         print(steps)
         if num!=0:
@@ -31,3 +31,19 @@ class Solution(object):
         :rtype: int
         """
         return self.recursive(num,0)
+
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        steps = 0
+        while num != 0:
+            if num%2 == 0:
+                num = num/2
+                steps+=1
+            else:
+                num -= 1
+                steps += 1
+        return steps
