1ce754f576dba54287d1ececcd1248da2ffbb041
leo <leonarss@stude.ntnu.no>
2025-07-04 09:44:02 +0200

    1342 added python solutions

--- Easy/1342.py
+++ Easy/1342.py
@@ -1,11 +1,11 @@
-class PythonSolution(object):
+class PythonRecursiveSolution(object):
     def recursive(self,num,steps):
         print(steps)
         if num > 0:
             if num%2==0:
-                steps = self.recursive(PythonSolution,num/2,steps+1)
+                steps = self.recursive(PythonRecursiveSolution,num/2,steps+1)
             else:
-                steps = self.recursive(PythonSolution,num-1,steps+1)
+                steps = self.recursive(PythonRecursiveSolution,num-1,steps+1)
         return steps
 
     def numberOfSteps(self, num):
@@ -13,7 +13,39 @@ class PythonSolution(object):
         :type num: int
         :rtype: int
         """
-        return self.recursive(PythonSolution,num,0)
+        return self.recursive(PythonRecursiveSolution,num,0)
+
+class PythonSolution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        steps = 0
+        while num > 0:
+            if num%2 == 0:
+                num /= 2
+                steps+=1
+            else:
+                num -= 1
+                steps += 1
+        return steps
+    
+class PythonBitwiseSolution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        steps = 0
+        while num > 0:
+            if (num&1)==0:
+                num >>= 1
+                steps+=1
+            else:
+                num -= 1
+                steps += 1
+        return steps
     
 class RecursiveSolution(object):
     def recursive(self,num,steps):
