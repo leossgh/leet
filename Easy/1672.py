ce52ce6df0c3110249525285fc9d94876a5622e3
leo <leonarss@stude.ntnu.no>
2025-07-04 08:43:47 +0200

    1672

---
+++ Easy/1672.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        richest = [0,0]
+        for i in range(len(accounts)):
+            if sum(accounts[i])>richest[1]:
+                richest = [i,sum(accounts[i])]
+        return richest[1]
