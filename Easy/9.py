3d38ab72bc95b29cc867a8be7c74c52b578946cc
leo <leonarss@stude.ntnu.no>
2025-07-04 13:04:01 +0200

    9 solution with string

---
+++ Easy/9.py
@@ -0,0 +1,20 @@
+class SimpleSolution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        else:
+            return x == int(str(x)[::-1])
+class PythonSimpleSolution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        else:
+            return x == int(str(x)[::-1])
