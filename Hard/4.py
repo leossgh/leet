0cbba32f556014742d91e24c0efec304e101293f
leo <leonardsandlokkschiller@gmail.com>
2025-07-07 14:36:43 +0200

    added third party time efficient solution

--- Hard/4.py
+++ Hard/4.py
@@ -55,6 +55,14 @@ class Solution(object):
             med = new_arr[-1]
         return med
 
-
-        #find min which is bigger than mean from both arrs
-        #find max which is smaller than mean from both arrs
+class Solution(object):
+    def TimefindMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums = sorted(nums1 + nums2) 
+        length = len(nums)
+        mid = length // 2
+        return float(nums[mid] if length % 2 else (nums[mid-1] + nums[mid]) / 2.0)
